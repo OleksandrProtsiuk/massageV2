a9c575eb3159395667a420bdb7a01e50f3ff68cd
alex <alex_is_ok@yahoo.com>
2019-05-14 23:48:35 +0300

    contacts page elements added-2 / maps helper created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def maps
+    key = Rails.application.credentials.maps[:api_key]
+    address = 'ул.+Крещатик,+195,+Черкассы'
+    "https://www.google.com/maps/embed/v1/place?key=#{ key }&q=#{ address }"
+  end
+
 end
