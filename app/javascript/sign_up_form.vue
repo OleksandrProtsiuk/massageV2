f18787cd810cc160b8b9375fbcc3973c061b7f8b
alex <alex_is_ok@yahoo.com>
2019-05-16 23:04:33 +0300

    form-component edited...

--- app/javascript/sign_up_form.vue
+++ app/javascript/sign_up_form.vue
@@ -28,9 +28,6 @@
             <b-button type="submit" variant="primary">Submit</b-button>
             <b-button type="reset" variant="danger">Reset</b-button>
         </b-form>
-        <b-card class="mt-3" header="Form Data Result">
-            <pre class="m-0">{{ form }}</pre>
-        </b-card>
     </div>
 </template>
 
@@ -42,11 +39,9 @@
                 form: {
                     email: '',
                     name: '',
-                    food: null,
-                    checked: []
+
                 },
-                foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-                show: true
+
             }
         },
         methods: {
@@ -59,8 +54,6 @@
                 // Reset our form values
                 this.form.email = '';
                 this.form.name = '';
-                this.form.food = null;
-                this.form.checked = [];
                 // Trick to reset/clear native browser form validation state
                 this.show = false;
                 this.$nextTick(() => {
